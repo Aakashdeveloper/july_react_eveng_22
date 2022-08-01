795038b80770a8d74222a056dcd055f2523384be
aakashdeveloper <ahanda205@gmail.com>
2022-08-01 21:54:43 +0530

    component

---
+++ firstapp/src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <center>
+                <h1>Developer Funnel</h1>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './component/Header';
+import Footer from './component/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Hiii From React</h1>
+            <h2>Developer Funnel</h2>
+            <Footer/>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container =  document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>)
+
+
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+
+// function App(){
+//     return(
+//         <h1>Hiii From React</h1>
+//     )
+// }
+
+// ReactDOM.render(<App/>, document.getElementById('root'))
