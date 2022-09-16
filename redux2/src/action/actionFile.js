612b9166d636e30bbdf697e1b833a86fb22c5b98
aakashdeveloper <ahanda205@gmail.com>
2022-09-16 21:30:30 +0530

    redux2

---
+++ redux2/src/action/actionFile.js
@@ -0,0 +1,31 @@
+const base_url = "http://localhost:7800/";
+
+export function latestNews(){
+    const output = fetch(`${base_url}/articles?_end=3`,{method:'GET'})
+    .then((data) => data.json())
+
+    return{
+        type:'LATEST_NEWS',
+        payload:output
+    }
+}
+
+export function articleNews(){
+    const output = fetch(`${base_url}/articles?_start=3&_end=10`,{method:'GET'})
+    .then((data) => data.json())
+
+    return{
+        type:'ARTICLE_NEWS',
+        payload:output
+    }
+}
+
+export function galleriesNews(){
+    const output = fetch(`${base_url}/galleries?_limit=2`,{method:'GET'})
+    .then((data) => data.json())
+
+    return{
+        type:'GALLERY_NEWS',
+        payload:output
+    }
+}
